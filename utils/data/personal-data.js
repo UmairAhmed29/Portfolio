afae6d2fd3e444dfdc5802dfadc8e6f90b82ee3c
UmairAhmed29 <umairahmed5544@gmail.com>
2024-11-15 18:25:25 +0500

    image fixed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,18 +1,18 @@
-
 export const personalData = {
   name: "Umair Ahmed",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "Software Developer",
-  description: "My name is Umair Ahmed. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore AI and machine learning world and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on MERN. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'umairahmed5544@gmail.com',
-  phone: '+923022081454',
-  address: 'R-364, Sector 14-b, Shadman Town, North Nazimabad, Karachi ',
-  github: 'https://github.com/UmairAhmed29',
-  facebook: 'https://www.facebook.com/umair.ahmed.50999/',
-  linkedIn: 'https://www.linkedin.com/in/muhammad-umair-ahmed-khan/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/26857502/umair-ahmed',
+  description:
+    "I am Umair Ahmed, a dedicated and passionate programmer with a strong foundation in web application development. As a quick learner with a self-driven attitude, I continuously explore the realms of artificial intelligence and machine learning, combining innovation with practical problem-solving. My expertise lies in the MERN stack, enabling me to build dynamic, user-centric applications that enhance the web's accessibility and usability.",
+  email: "umairahmed5544@gmail.com",
+  phone: "+923022081454",
+  address: "R-364, Sector 14-b, Shadman Town, North Nazimabad, Karachi ",
+  github: "https://github.com/UmairAhmed29",
+  facebook: "https://www.facebook.com/umair.ahmed.50999/",
+  linkedIn: "https://www.linkedin.com/in/muhammad-umair-ahmed-khan/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/26857502/umair-ahmed",
   leetcode: "hhttps://leetcode.com/u/rhoND1NUui/",
   devUsername: "UmairAhmed29",
-  resume: "/resume/UmairAhmed-SoftwareEngineer.pdf"
-}
+  resume: "/resume/UmairAhmed-SoftwareEngineer.pdf",
+};
