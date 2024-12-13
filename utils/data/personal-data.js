b8a614ca3a0ff0b43ac03c97994c1692549277fc
Umair Ahmed <91034129+UmairAhmed29@users.noreply.github.com>
2024-12-13 17:35:57 +0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "Software Developer",
   description:
-    "I am Umair Ahmed, a dedicated and passionate programmer with a strong foundation in web application development. As a quick learner with a self-driven attitude, I continuously explore the realms of artificial intelligence and machine learning, combining innovation with practical problem-solving. My expertise lies in the MERN stack, enabling me to build dynamic, user-centric applications that enhance the web's accessibility and usability.",
+    "Hey, I'm Umair Ahmed—a programmer who loves turning ideas into awesome web applications. With a solid grip on the MERN stack, I enjoy creating dynamic, user-friendly solutions that not only work great but feel great to use. I’m always up for a challenge and constantly diving into cool stuff like AI and machine learning to push boundaries and find creative ways to solve problems. Whether it's building sleek apps or experimenting with the latest tech, I’m all about making the web a more exciting and accessible place for everyone.",
   email: "umairahmed5544@gmail.com",
   phone: "+923022081454",
   address: "R-364, Sector 14-b, Shadman Town, North Nazimabad, Karachi ",
